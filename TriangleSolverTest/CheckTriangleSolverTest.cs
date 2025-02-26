c57457e652dfb20c873dd60ebdb243181bfeb65e
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-02-25 20:11:45 -0500

    Equilateral Triangle

--- TriangleSolverTest/CheckTriangleSolverTest.cs
+++ TriangleSolverTest/CheckTriangleSolverTest.cs
@@ -5,25 +5,11 @@ namespace TriangleSolverTests
 {
     public class CheckTriangleSolverTest
     {
-      [Test]
-        public void Test_Isosceles_Triangle_Case1()
+       [Test]
+        public void Test_Equilateral_Triangle()
         {
-            string result = Triangle.AnalyzeTriangle(5, 5, 3);
-            Assert.That(result, Is.EqualTo("Isosceles triangle"));
-        }
-
-        [Test]
-        public void Test_Isosceles_Triangle_Case2()
-        {
-            string result = Triangle.AnalyzeTriangle(4, 4, 6);
-            Assert.That(result, Is.EqualTo("Isosceles triangle"));
-        }
-
-        [Test]
-        public void Test_Isosceles_Triangle_Case3()
-        {
-            string result = Triangle.AnalyzeTriangle(7, 9, 9);
-            Assert.That(result, Is.EqualTo("Isosceles triangle"));
+            string result = Triangle.AnalyzeTriangle(5, 5, 5);
+            Assert.That(result, Is.EqualTo("Equilateral triangle"));
         }
     }
 }
