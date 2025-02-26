0ba4bf349c9730933c537d4197700b65bf28c250
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-02-25 19:23:45 -0500

    Test case 3

--- TriangleSolverTest/CheckTriangleSolverTest.cs
+++ TriangleSolverTest/CheckTriangleSolverTest.cs
@@ -6,16 +6,41 @@ namespace TriangleSolverTests
     public class CheckTriangleSolverTest
     {
         [Test]
-        public void Test_Valid_Equilateral_Triangle()
+        public void Test_Valid_Isosceles_Triangle_1()
         {
-            // Arrange
-            int side1 = 5, side2 = 5, side3 = 5;
+            /
+            int side1 = 5, side2 = 5, side3 = 3;
 
-            // Act
+          
             string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-            // Assert
-            Assert.That(result, Is.EqualTo("Equilateral"));
+            
+            Assert.That(result, Is.EqualTo("Isosceles"));
         }
+
+        [Test]
+        public void Test_Valid_Isosceles_Triangle_2()
+        {
+          
+            int side1 = 6, side2 = 6, side3 = 4;
+
+         
+            string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
+
+           
+            Assert.That(result, Is.EqualTo("Isosceles"));
+        }
+
+        [Test]
+        public void Test_Valid_Isosceles_Triangle_3()
+        {
+           
+            int side1 = 7, side2 = 7, side3 = 5;
+
+            string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
+
+            Assert.That(result, Is.EqualTo("Isosceles"));
+        }
+
     }
 }
