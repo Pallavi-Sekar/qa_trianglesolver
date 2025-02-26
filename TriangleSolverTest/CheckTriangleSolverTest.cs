a3a8d73a2ebbe605a409294e49c02eab173f7594
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-02-25 19:21:55 -0500

    Test case 2

--- TriangleSolverTest/CheckTriangleSolverTest.cs
+++ TriangleSolverTest/CheckTriangleSolverTest.cs
@@ -6,66 +6,16 @@ namespace TriangleSolverTests
     public class CheckTriangleSolverTest
     {
         [Test]
-        public void Test_Equilateral_Triangle()
+        public void Test_Valid_Equilateral_Triangle()
         {
-            string result = Triangle.AnalyzeTriangle(5, 5, 5);
-            Assert.That(result, Is.EqualTo("Equilateral triangle"));
-        }
+            // Arrange
+            int side1 = 5, side2 = 5, side3 = 5;
 
-        [Test]
-        public void Test_Isosceles_Triangle()
-        {
-            string result = Triangle.AnalyzeTriangle(5, 5, 3);
-            Assert.That(result, Is.EqualTo("Isosceles triangle"));
-        }
+            // Act
+            string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-        [Test]
-        public void Test_Scalene_Triangle()
-        {
-            string result = Triangle.AnalyzeTriangle(3, 4, 5);
-            Assert.That(result, Is.EqualTo("Scalene triangle"));
-        }
-
-        [Test]
-        public void Test_Invalid_Zero_Side()
-        {
-            string result = Triangle.AnalyzeTriangle(0, 3, 3);
-            Assert.That(result, Is.EqualTo("Invalid Triangle - a zero has been detected"));
-        }
-
-        [Test]
-        public void Test_Invalid_Negative_Side()
-        {
-            string result = Triangle.AnalyzeTriangle(-2, 3, 3);
-            Assert.That(result, Is.EqualTo("Invalid Triangle - a zero has been detected"));
-        }
-
-        [Test]
-        public void Test_Invalid_Triangle_Inequality()
-        {
-            string result = Triangle.AnalyzeTriangle(1, 2, 3);
-            Assert.That(result, Is.EqualTo("INVALID!!"));
-        }
-
-        [Test]
-        public void Test_Invalid_Triangle_Inequality_LargeSide()
-        {
-            string result = Triangle.AnalyzeTriangle(10, 2, 2);
-            Assert.That(result, Is.EqualTo("INVALID!!"));
-        }
-
-        [Test]
-        public void Test_Invalid_Negative_Side_Triangle_Inequality()
-        {
-            string result = Triangle.AnalyzeTriangle(5, -1, 3);
-            Assert.That(result, Is.EqualTo("Invalid Triangle - a zero has been detected"));
-        }
-
-        [Test]
-        public void Test_Invalid_Zero_Side_AnotherCombination()
-        {
-            string result = Triangle.AnalyzeTriangle(3, 0, 4);
-            Assert.That(result, Is.EqualTo("Invalid Triangle - a zero has been detected"));
+            // Assert
+            Assert.That(result, Is.EqualTo("Equilateral"));
         }
     }
 }
