6069e9ec2231f758415d5cf11f2d2a4545733a5e
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-02-25 19:32:59 -0500

    Test case 5

--- TriangleSolverTest/CheckTriangleSolverTest.cs
+++ TriangleSolverTest/CheckTriangleSolverTest.cs
@@ -6,34 +6,35 @@ namespace TriangleSolverTests
     public class CheckTriangleSolverTest
     {
         [Test]
-        public void Test_Zero_Length_Side_1()
+        public void Test_Invalid_Triangle_Inequality_1()
         {
-            int side1 = 0, side2 = 5, side3 = 5;
+            int side1 = 1, side2 = 2, side3 = 3;
 
             string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-            Assert.That(result, Is.EqualTo("Invalid - Zero or Negative Side"));
+            Assert.That(result, Is.EqualTo("Invalid - Triangle Inequality"));
         }
 
         [Test]
-        public void Test_Zero_Length_Side_2()
+        public void Test_Invalid_Triangle_Inequality_2()
         {
-            int side1 = 3, side2 = 0, side3 = 5;
+            int side1 = 10, side2 = 2, side3 = 2;
 
             string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-            Assert.That(result, Is.EqualTo("Invalid - Zero or Negative Side"));
+            Assert.That(result, Is.EqualTo("Invalid - Triangle Inequality"));
         }
 
         [Test]
-        public void Test_Zero_Length_Side_3()
+        public void Test_Invalid_Triangle_Inequality_3()
         {
-            int side1 = 5, side2 = 5, side3 = 0;
+            int side1 = 7, side2 = 5, side3 = 13;
 
             string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-            Assert.That(result, Is.EqualTo("Invalid - Zero or Negative Side"));
+            Assert.That(result, Is.EqualTo("Invalid - Triangle Inequality"));
         }
+
 
 
 
