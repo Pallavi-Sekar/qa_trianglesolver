b4300dfa873fc7452ebd658ff096c2b4474f0edd
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-02-25 19:27:47 -0500

    Test case 4

--- TriangleSolverTest/CheckTriangleSolverTest.cs
+++ TriangleSolverTest/CheckTriangleSolverTest.cs
@@ -6,59 +6,35 @@ namespace TriangleSolverTests
     public class CheckTriangleSolverTest
     {
         [Test]
-        public void Test_Valid_Scalene_Triangle_1()
+        public void Test_Zero_Length_Side_1()
         {
-            int side1 = 3, side2 = 4, side3 = 5;
+            int side1 = 0, side2 = 5, side3 = 5;
 
             string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-            Assert.That(result, Is.EqualTo("Scalene"));
+            Assert.That(result, Is.EqualTo("Invalid - Zero or Negative Side"));
         }
 
         [Test]
-        public void Test_Valid_Scalene_Triangle_2()
+        public void Test_Zero_Length_Side_2()
         {
-            int side1 = 8, side2 = 6, side3 = 7;
+            int side1 = 3, side2 = 0, side3 = 5;
 
             string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-
-            Assert.That(result, Is.EqualTo("Scalene"));
+            Assert.That(result, Is.EqualTo("Invalid - Zero or Negative Side"));
         }
 
         [Test]
-        public void Test_Valid_Scalene_Triangle_3()
+        public void Test_Zero_Length_Side_3()
         {
-            int side1 = 9, side2 = 4, side3 = 12;
+            int side1 = 5, side2 = 5, side3 = 0;
 
             string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
 
-            Assert.That(result, Is.EqualTo("Scalene"));
+            Assert.That(result, Is.EqualTo("Invalid - Zero or Negative Side"));
         }
 
-        [Test]
-        public void Test_Valid_Scalene_Triangle_4()
-        {
-
-            int side1 = 15, side2 = 11, side3 = 9;
-
-            string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
-
-            Assert.That(result, Is.EqualTo("Scalene"));
-        }
-
-        [Test]
-        public void Test_Valid_Scalene_Triangle_5()
-        {
-
-            int side1 = 20, side2 = 25, side3 = 30;
-
-
-            string result = TriangleSolver.TriangleSolver.GetTriangleType(side1, side2, side3);
-
-
-            Assert.That(result, Is.EqualTo("Scalene"));
-        }
 
 
     }
