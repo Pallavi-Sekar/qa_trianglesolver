4a2a2013396e72909df3201843eabeaa15991050
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-02-25 20:09:35 -0500

    Scalene Triangle

--- TriangleSolverTest/CheckTriangleSolverTest.cs
+++ TriangleSolverTest/CheckTriangleSolverTest.cs
@@ -6,24 +6,38 @@ namespace TriangleSolverTests
     public class CheckTriangleSolverTest
     {
       [Test]
-        public void Test_Invalid_Zero_Side_Case1()
+        public void Test_Scalene_Triangle_Case1()
         {
-            string result = Triangle.AnalyzeTriangle(0, 3, 3);
-            Assert.That(result, Is.EqualTo("Invalid Triangle - a zero has been detected"));
+            string result = Triangle.AnalyzeTriangle(3, 4, 5);
+            Assert.That(result, Is.EqualTo("Scalene triangle"));
         }
 
         [Test]
-        public void Test_Invalid_Zero_Side_Case2()
+        public void Test_Scalene_Triangle_Case2()
         {
-            string result = Triangle.AnalyzeTriangle(3, 0, 4);
-            Assert.That(result, Is.EqualTo("Invalid Triangle - a zero has been detected"));
+            string result = Triangle.AnalyzeTriangle(6, 8, 10);
+            Assert.That(result, Is.EqualTo("Scalene triangle"));
         }
 
         [Test]
-        public void Test_Invalid_Zero_Side_Case3()
+        public void Test_Scalene_Triangle_Case3()
         {
-            string result = Triangle.AnalyzeTriangle(4, 5, 0);
-            Assert.That(result, Is.EqualTo("Invalid Triangle - a zero has been detected"));
+            string result = Triangle.AnalyzeTriangle(7, 11, 13);
+            Assert.That(result, Is.EqualTo("Scalene triangle"));
+        }
+
+        [Test]
+        public void Test_Scalene_Triangle_Case4()
+        {
+            string result = Triangle.AnalyzeTriangle(9, 12, 15);
+            Assert.That(result, Is.EqualTo("Scalene triangle"));
+        }
+
+        [Test]
+        public void Test_Scalene_Triangle_Case5()
+        {
+            string result = Triangle.AnalyzeTriangle(5, 7, 9);
+            Assert.That(result, Is.EqualTo("Scalene triangle"));
         }
     }
 }
