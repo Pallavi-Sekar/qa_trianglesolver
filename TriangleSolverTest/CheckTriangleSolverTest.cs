8fd9fb3f90a96044f4ea12e3549ef0440e86859a
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-02-25 20:10:24 -0500

    Isoceles Triangle

--- TriangleSolverTest/CheckTriangleSolverTest.cs
+++ TriangleSolverTest/CheckTriangleSolverTest.cs
@@ -6,38 +6,24 @@ namespace TriangleSolverTests
     public class CheckTriangleSolverTest
     {
       [Test]
-        public void Test_Scalene_Triangle_Case1()
+        public void Test_Isosceles_Triangle_Case1()
         {
-            string result = Triangle.AnalyzeTriangle(3, 4, 5);
-            Assert.That(result, Is.EqualTo("Scalene triangle"));
+            string result = Triangle.AnalyzeTriangle(5, 5, 3);
+            Assert.That(result, Is.EqualTo("Isosceles triangle"));
         }
 
         [Test]
-        public void Test_Scalene_Triangle_Case2()
+        public void Test_Isosceles_Triangle_Case2()
         {
-            string result = Triangle.AnalyzeTriangle(6, 8, 10);
-            Assert.That(result, Is.EqualTo("Scalene triangle"));
+            string result = Triangle.AnalyzeTriangle(4, 4, 6);
+            Assert.That(result, Is.EqualTo("Isosceles triangle"));
         }
 
         [Test]
-        public void Test_Scalene_Triangle_Case3()
+        public void Test_Isosceles_Triangle_Case3()
         {
-            string result = Triangle.AnalyzeTriangle(7, 11, 13);
-            Assert.That(result, Is.EqualTo("Scalene triangle"));
-        }
-
-        [Test]
-        public void Test_Scalene_Triangle_Case4()
-        {
-            string result = Triangle.AnalyzeTriangle(9, 12, 15);
-            Assert.That(result, Is.EqualTo("Scalene triangle"));
-        }
-
-        [Test]
-        public void Test_Scalene_Triangle_Case5()
-        {
-            string result = Triangle.AnalyzeTriangle(5, 7, 9);
-            Assert.That(result, Is.EqualTo("Scalene triangle"));
+            string result = Triangle.AnalyzeTriangle(7, 9, 9);
+            Assert.That(result, Is.EqualTo("Isosceles triangle"));
         }
     }
 }
